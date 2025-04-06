fd96298446d383ef4f55d7898daaf6bb91e11054
Aly-812 <alice.casini.93@hotmail.it>
2025-04-06 20:12:48 +0200

    completate le funzioni e aggiunti i test

---
+++ test_area_quadrato.py
@@ -0,0 +1,5 @@
+from area_quadrato import area_quadrato
+
+def test_area_quadrato():
+    area_prova = area_quadrato(5)
+    assert area_prova == 25
---
+++ test_area_triangolo.py
@@ -0,0 +1,5 @@
+from area_triangolo import area_triangolo
+
+def test_area_triangolo():
+    area_prova = area_triangolo(10,4)
+    assert area_prova == 20
